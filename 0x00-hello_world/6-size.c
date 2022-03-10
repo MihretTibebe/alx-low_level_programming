c9d02009ae0e6cce02587baeb524e4a3ffbaa23c
MihretTibebe <mihret.tibe@gmail.com>
2022-03-10 15:41:26 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+puts("Size of a char: %d byte(s)", sizeof(char));
+puts("Size of an int: %d byte(s)", sizeof(int));
+puts("Size of a long int: %d byte(s)", sizeof(long));
+puts("Size of a long long int: %d byte(s)", sizeof(long long int));
+puts("Size of a float: %d byte(s)", sizeof(float));
+return (0);
