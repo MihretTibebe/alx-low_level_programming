afb1dc7115e2d8bb939e3772d3f3812ea9fb5f38
MihretTibebe <mihret.tibe@gmail.com>
2022-03-10 16:26:15 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,10 @@
  */
 int main(void)
 {
-puts("Size of a char: %d byte(s)", sizeof(char));
-puts("Size of an int: %d byte(s)", sizeof(int));
-puts("Size of a long int: %d byte(s)", sizeof(long));
-puts("Size of a long long int: %d byte(s)", sizeof(long long int));
-puts("Size of a float: %d byte(s)", sizeof(float));
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of an int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of a float: %i byte(s)\n", sizeof(float));
 return (0);
+}
