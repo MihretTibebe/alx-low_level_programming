c15042d88981719f93fbf93f68dd1f4fe1ed5d06
MihretTibebe <mihret.tibe@gmail.com>
2022-03-11 17:13:16 -0800

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - start piont
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 48;
+while (i <= 57)
+{
+putchar(i);
+i++;
+}
+int j = 97;
+while (j <= 102)
+{
+putchar(j);
+j++;
+}
+putchar(10);
+return (0);
+}
+
