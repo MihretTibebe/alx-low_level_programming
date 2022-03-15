c65c8f957075b7a71d5f93b9fe09f34936cf5828
MihretTibebe <mihret.tibe@gmail.com>
2022-03-15 16:35:35 -0700

    functions

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c : character to be checked
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+if (c == i)
+return (1);
+}
+int i;
+for (i = 65; i <= 90; i++)
+{
+if (c == i)
+return (1);
+}
+return (0);
+}
+
