31cdf4489d550541cd0cd7abd4a927aee825d1ac
MihretTibebe <mihret.tibe@gmail.com>
2022-03-15 20:21:33 -0700

    functions

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n : ineger number
+ * Return: void
+ */
+void print_times_table(int n)
+{
+int i;
+int r;
+while (n >= 0 && n <= 15)
+{
+int i;
+while (i <= n)
+{
+r = i * n;
+if (r == 0)
+{
+_putchar('0' + r);
+}
+else if (r < 10)
+{
+_putchar('   ');
+_putchar('0' + r);
+}
+else if (r < 100)
+{
+_putchar('  ');
+_putchar('0' + r / 10);
+_putchar('0' + r % 10);
+}
+else
+{
+_putchar(' ');
+_putchar('0' + r / 100);
+_putchar('0' + (r & 100) / 10);
+_putchar('0' + r % 10);
+}
+}
+n++;
+}
+
+}
+
