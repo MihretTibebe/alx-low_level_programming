d6915c981a6d781a86f12089140f140ba87f98f1
MihretTibebe <mihret.tibe@gmail.com>
2022-03-24 14:58:30 -0700

    pointer

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s : string to be converted
+ *
+ * Return: string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') &&
+					s[i] == in[j])
+				s[i] = out[j]
+		}
+	}
+}
