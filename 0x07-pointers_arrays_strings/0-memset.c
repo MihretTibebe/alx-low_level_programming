f43a28e95b0257a4ca4e22be7966e5f224840553
MihretTibebe <mihret.tibe@gmail.com>
2022-03-29 06:59:03 -0700

    pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * _memset - fills memory with a constant byte
  * @s : string to be chenged;
@@ -9,7 +9,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
