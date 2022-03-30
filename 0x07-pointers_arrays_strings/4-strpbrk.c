d864201937a6bf5c89f0507cc9715f9bcc48e9f5
MihretTibebe <mihret.tibe@gmail.com>
2022-03-30 04:05:44 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *_strpbrk - gets the length of a prefix substring
+ *
+ * @s : string to be searched
+ * @accept : accept string
+ * Return: int
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	int count = 0;
+	char *r;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				r = &s[i];
+				return (r);
+			}
+		}
+	}
+}
