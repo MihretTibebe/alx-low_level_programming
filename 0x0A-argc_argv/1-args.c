d61e505df73dd3e59fdd68f52f1ede4ee8de658e
MihretTibebe <mihret.tibe@gmail.com>
2022-04-01 12:50:27 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - start point
+ * @argc : int
+ * @argv : char
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	if (argv != NULL)
+		printf("%d\n", argc);
+	return (0);
+}
