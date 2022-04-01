ab2dfabe6fdee63976183ef8e24450e962e2815f
MihretTibebe <mihret.tibe@gmail.com>
2022-04-01 12:38:40 -0700

    args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - start point
+ * @argc : int
+ * @argv : char
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
+
