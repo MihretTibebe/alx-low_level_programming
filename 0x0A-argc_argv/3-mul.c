4ded8e189ef0586daf991a828b12fd3bb2a17f9f
MihretTibebe <mihret.tibe@gmail.com>
2022-04-01 13:13:47 -0700

    args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - start point
+ * @argc : int
+ * @argv : char
+ * Return 0 or 1
+ */
+
+int main(int argc, char *argv)
+{
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+	{
+		printf("%s\n", Error);
+		return (1);
+	}
+
+}
