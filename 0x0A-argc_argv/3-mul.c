c320fdd8530df5ce95a4799c0c8c68b839abc454
MihretTibebe <mihret.tibe@gmail.com>
2022-04-01 13:20:25 -0700

    args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,16 +7,19 @@
  * Return 0 or 1
  */
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
+	int mul;
+
 	if (argc == 3)
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
 		return (0);
 	}
 	else
 	{
-		printf("%s\n", Error);
+		printf("%s\n", "Error");
 		return (1);
 	}
 
