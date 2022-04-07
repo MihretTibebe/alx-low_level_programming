245bfec85d5ac82290253739353a2f89b972e903
MihretTibebe <mihret.tibe@gmail.com>
2022-04-07 14:50:18 -0700

    malloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * _memset - set a memory
+ * @p : char
+ * @a : char
+ * @n : int
+ * Return: char
+ */
+
+char *_memset(char *p, char *a, int n)
+{
+	char *s = p;
+
+	while (n--)
+	{
+		*s++ = *a++;
+	}
+return (s);
+}
+
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr : void pointer
+ * @old_size : unsigned int
+ * @new_size : unsigned int
+ * Return: pointer
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+	int i, j;
+
+	if (old_size == new_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		return (p);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (ptr);
+	}
+	if (new_size > old_size)
+	{
+		p = malloc(new_size);
+		_memset(p, ptr, old_size);
+	}
+return (p);
+}
