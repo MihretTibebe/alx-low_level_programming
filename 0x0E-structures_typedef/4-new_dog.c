5a027b49ff1a6cbbd284c2aeb6035adf61809b85
MihretTibebe <mihret.tibe@gmail.com>
2022-04-11 15:41:31 -0700

    submit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,7 +15,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d = malloc(sizeof(dog_t));
 
 	if (d == 0)
+	{
+		free(d);
 		return (NULL);
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
