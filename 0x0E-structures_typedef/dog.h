cff9649c32d8053b326531067fefd696aa8598b7
MihretTibebe <mihret.tibe@gmail.com>
2022-04-11 14:30:18 -0700

    submit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - short description
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Discription: Longer description
+ */
+
 struct dog
 {
 	char *name;
 	float *age;
 	char *owner;
 };
+
+#endif /* DOG_H */
