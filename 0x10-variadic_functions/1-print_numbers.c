5f0585d31395e619833428c7bd6cb6d3eeace195
MihretTibebe <mihret.tibe@gmail.com>
2022-04-18 14:58:14 -0700

    add

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-	
+
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
@@ -28,6 +28,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%s", separator);
 	}
 	printf("\n");
-	
+
 }
 
