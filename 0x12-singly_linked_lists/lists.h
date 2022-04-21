2fd93dcc6794997b3858fa3a615888e6757f8c0b
MihretTibebe <mihret.tibe@gmail.com>
2022-04-21 14:16:19 -0700

    submit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
-#define LISTS_H
 #ifndef LISTS_H
-
+#define LISTS_H
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -9,11 +8,12 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char ch);
